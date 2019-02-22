1146d5d83bde6907493965b4f94417549c9ddd4e
VitorBMaia <VitorBMaia>
2019-02-22 16:53:31 -0300

    Exercicio codando em dupla

---
+++ case-transacao/Debito.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+
+namespace case_transacao
+{
+    public class Debito : TransacaoDePagamento
+    {
+        public Debito(double valorTransacao, List<Transacao> lista)
+        {  
+            if(valorTransacao <= 0) 
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+            this.IdTransacao = 20000000 + ++Transacao.QuantidadeDeTransacoes;
+            this.ValorTransacao = valorTransacao;
+            this.Estado = base.Autorizacao();
+            }
+
+
+            lista.Add(this);
+        }
+
+        public Debito(double valorTransacao)
+        {
+            this.ValorTransacao = valorTransacao;
+            this.Estado = base.Autorizacao();
+        }
+    }
+
+ 
+}
